89922c2298d993a9bb5657699127350c1fe46221
Nikolay Chernev <nikolay.chernev@dreamix.eu>
2022-08-26 09:49:25 +0300

    Remove await statements.

--- custom_components/basestation/switch.py
+++ custom_components/basestation/switch.py
@@ -39,15 +39,15 @@ class BasestationSwitch(SwitchEntity):
 
     def turn_on(self, **kwargs):
         """Turn the switch on."""
-        await self._device.connect()
-        await self._device.write_gatt_char(PWR_CHARACTERISTIC, PWR_ON)
-        await self._device.disconnect()
+        self._device.connect()
+        self._device.write_gatt_char(PWR_CHARACTERISTIC, PWR_ON)
+        self._device.disconnect()
 
     def turn_off(self, **kwargs):
         """Turn the switch off."""
-        await self._device.connect()
-        await self._device.write_gatt_char(PWR_CHARACTERISTIC, PWR_STANDBY)
-        await self._device.disconnect()
+        self._device.connect()
+        self._device.write_gatt_char(PWR_CHARACTERISTIC, PWR_STANDBY)
+        self._device.disconnect()
 
     @property
     def name(self):
@@ -59,6 +59,6 @@ class BasestationSwitch(SwitchEntity):
 
     def update(self):
         """Fetch new state data for the sensor."""
-        await self._device.connect()
-        self._is_on = await self._device.read_gatt_char(PWR_CHARACTERISTIC) != PWR_STANDBY
-        await self._device.disconnect()
+        self._device.connect()
+        self._is_on = self._device.read_gatt_char(PWR_CHARACTERISTIC) != PWR_STANDBY
+        self._device.disconnect()
