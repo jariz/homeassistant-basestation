140ea1c043b90dcbdb157191e132c1c8b6172546
lublak <44057030+lublak@users.noreply.github.com>
2024-07-14 13:37:03 +0200

    add available check
    fix Bleak error, if the device is off (power)

--- custom_components/basestation/switch.py
+++ custom_components/basestation/switch.py
@@ -1,6 +1,9 @@
 """The basestation switch."""
 
-from bleak import BleakClient
+from bleak import (
+    BleakClient,
+    BleakError
+)
 from homeassistant.components.switch import SwitchEntity
 from homeassistant.components import bluetooth
 import asyncio
@@ -16,7 +19,7 @@ def setup_platform(hass, config, add_entities, discovery_info=None):
     """Set up the sensor platform."""
     mac = config.get("mac")
     name = config.get("name")
-    add_entities([BasestationSwitch(hass, mac, name)])
+    add_entities([BasestationSwitch(hass, mac, name)], update_before_add=True)
 
 
 class BasestationSwitch(SwitchEntity):
@@ -28,11 +31,21 @@ class BasestationSwitch(SwitchEntity):
         self._mac = mac
         self._name = name
         self._is_on = False
+        self._is_available = False
 
     @property
     def icon(self):
         """Return the icon."""
         return "mdi:virtual-reality"
+
+    @property
+    def should_poll(self):
+        return True
+
+    @property
+    def available(self):
+        """Return the connection status of this switch"""
+        return self._is_available
 
     @property
     def is_on(self):
@@ -68,8 +81,14 @@ class BasestationSwitch(SwitchEntity):
             await client.write_gatt_char(PWR_CHARACTERISTIC, PWR_STANDBY)
 
     async def async_update(self):
-        async with BleakClient(self.get_ble_device()) as client:
-            self._is_on = await client.read_gatt_char(PWR_CHARACTERISTIC) != PWR_STANDBY
+        try:
+            async with BleakClient(self.get_ble_device()) as client:
+                self._is_on = await client.read_gatt_char(PWR_CHARACTERISTIC) != PWR_STANDBY
+                self._is_available = True
+        except BleakError:
+            self._is_on = False
+            self._is_available = False
+        
 
     def get_ble_device(self):
         return bluetooth.async_ble_device_from_address(self._hass, str(self._mac))
